4a5050313888b5cb3f72b0f2d7be81794a2ffc60
Cosiek <fitek2@o2.pl>
2024-01-13 22:10:30 +0100

    add initial html parser

---
+++ html_parser/parser.py
@@ -0,0 +1,167 @@
+import re
+
+NODE_TYPE_NAME_MATCHER = re.compile(r"<\/?([a-zA-Z-]+)([(\/>) ]|$)")
+
+
+# A void element is one that cannot have any child nodes
+VOID_TYPES = {
+    "area", "base", "br", "col", "embed",
+    "hr", "img", "input", "link", "meta",
+    "param", "source", "track", "wbr",
+}
+
+# Invisible types are ones that are never rendered by browsers
+# NOTE: this is not a full list
+INVISIBLE_TYPES = {
+    "audio", "head", "script", "style", "video",
+}
+
+
+class BaseNode:
+
+    def will_accept(self, buffer: str):
+        return False
+
+    def get_text(self):
+        return ""
+
+
+class HTMLNode(BaseNode):
+
+    def __init__(self, buffer: str):
+        self.type = get_node_type(buffer)
+
+        self.nodes = []
+        self.is_closed = False
+
+    def will_accept(self, buffer: str):
+        return not self.is_closed
+
+    def digest(self, buffer: str):
+        # check if last node will accept this buffer
+        if self.nodes and self.nodes[-1].will_accept(buffer):
+            self.nodes[-1].digest(buffer)
+        # or is it a closing for this tag
+        elif buffer.startswith(f"</{self.type}"):
+            self.is_closed = True
+        else:
+            self.nodes.append(get_new_node(buffer))
+
+    def get_text(self):
+        return "\n".join(filter(None, [n.get_text() for n in self.nodes])) if self.is_visible else ""
+
+    @property
+    def is_visible(self):
+        return self.type not in INVISIBLE_TYPES
+
+
+class VoidHTMLNode(BaseNode):
+
+    def __init__(self, buffer: str):
+        # void nodes can't contain text - no need to care much for them
+        self.type = get_node_type(buffer)
+
+    def will_accept(self, buffer: str):
+        return False
+
+
+class TextNode(BaseNode):
+
+    def __init__(self, buffer: str):
+        self.type = "text"
+        self.content = buffer
+
+    def will_accept(self, buffer: str):
+        return not buffer.startswith("<")
+
+    def digest(self, buffer: str):
+        self.content += buffer
+
+    def get_text(self):
+        return self.content
+
+
+class CommentNode(BaseNode):
+
+    def __init__(self, buffer: str):
+        self.type = "comment"
+        self.digest(buffer)
+
+    def will_accept(self, buffer: str):
+        return not self.is_closed
+
+    def digest(self, buffer: str):
+        self.is_closed = buffer.endswith("-->")
+
+
+class DoctypeNode(BaseNode):
+
+    def __init__(self, buffer: str):
+        self.type = "doctype"
+
+    def will_accept(self, buffer: str):
+        return False
+
+
+class RootNode:
+
+    def __init__(self):
+        self.type = "root"
+        self.nodes = []
+
+    def digest(self, buffer: str):
+        if self.nodes and self.nodes[-1].will_accept(buffer):
+            self.nodes[-1].digest(buffer)
+        else:
+            self.nodes.append(get_new_node(buffer))
+
+    def get_text(self):
+        return "\n".join(filter(None, [n.get_text() for n in self.nodes]))
+
+
+def get_node_type(buffer: str):
+    type_mach = NODE_TYPE_NAME_MATCHER.match(buffer)
+    if type_mach:
+        return type_mach.group(1)
+    return None
+
+
+def get_new_node(buffer: str):
+    if not buffer.startswith("<"):
+        cls = TextNode
+    elif buffer.startswith("<!--"):
+        cls = CommentNode
+    elif buffer.startswith("<!doctype"):
+        cls = DoctypeNode
+    else:
+        # these should be normal html tags
+        node_type = get_node_type(buffer)
+        if node_type in VOID_TYPES:
+            cls = VoidHTMLNode
+        elif node_type is None:
+            # this doesn't seem to be a proper html tag
+            # TODO: is ignoring the right thing to do here?
+            return None
+        else:
+            # this assumes that unknown tags are not void
+            cls = HTMLNode
+
+    return cls(buffer)
+
+
+def parse_html(html: str) -> RootNode:
+    root = RootNode()
+
+    buffer = ""
+    for letter in html:
+        if letter == "<":
+            if buffer.strip():  # no whitespace nodes allowed
+                root.digest(buffer)
+            buffer = "<"
+        elif letter == ">":
+            root.digest(buffer + letter)
+            buffer = ""
+        else:
+            buffer += letter
+
+    return root
---
+++ html_parser/tests.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from unittest import TestCase
+
+from parser import parse_html, get_node_type, get_new_node
+
+TEST_FILES_DIR = Path(__file__).parent / "test_files"
+
+
+class HTMLParsingTestCase(TestCase):
+
+    @staticmethod
+    def get_html(name):
+        with open(TEST_FILES_DIR / f"{name}.html") as f:
+            return f.read()
+
+    def test_base_html_parsing(self):
+        html = self.get_html("base")
+
+        root = parse_html(html)
+
+        self.assertEqual(root.type, "root")
+        self.assertEqual(len(root.nodes), 2)
+
+        doctype_node = root.nodes[0]
+        self.assertEqual(doctype_node.type, "doctype")
+
+        head_node = root.nodes[1]
+        self.assertEqual(head_node.type, "html")
+
+        self.assertEqual(root.get_text(), "Test text.")
+
+    def test_getting_node_types(self):
+        cases = [
+            ("p", "<p>"),
+            ("p", "<p style=\"xxx\">"),
+            ("br", "<br/>"),
+            ("br", "<br />"),
+        ]
+
+        for node_type, buffer in cases:
+            self.assertEqual(node_type, get_node_type(buffer),
+                             f"Error for buffer='{buffer}'")
+
+    def test_getting_new_node_instances(self):
+        cases = [
+            ("p", "<p>"),
+            ("p", "<p style=\"xxx\">"),
+            ("br", "<br/>"),
+            ("br", "<br />"),
+            ("comment", "<!-- THIS IS A COMMENT -->"),
+            ("comment", "<!-- THIS IS A \nMULTILINE\n COMMENT -->"),
+            ("doctype", "<!doctype html>"),
+            ("text", "abc"),
+            ("meta", "<meta charset=\"utf-8\" />"),
+        ]
+
+        for node_type, buffer in cases:
+            node = get_new_node(buffer)
+            self.assertIsNotNone(node.type, msg=f"Error for buffer='{buffer}")
+            self.assertEqual(node_type, node.type,
+                             f"Error for buffer='{buffer}'")
