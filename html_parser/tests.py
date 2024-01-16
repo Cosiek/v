9f86e930edd979363925af4598263f7e28c2a097
Cosiek <fitek2@o2.pl>
2024-01-16 22:25:48 +0100

    improve html parser tags splitting

--- html_parser/tests.py
+++ html_parser/tests.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 from unittest import TestCase
 
-from parser import parse_html, get_node_type, get_new_node
+from .parser import parse_html, get_node_type, get_new_node
 
 TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 
 class HTMLParsingTestCase(TestCase):
-
     @staticmethod
     def get_html(name):
         with open(TEST_FILES_DIR / f"{name}.html") as f:
@@ -51,31 +50,31 @@ class HTMLParsingTestCase(TestCase):
     def test_getting_node_types(self):
         cases = [
             ("p", "<p>"),
-            ("p", "<p style=\"xxx\">"),
+            ("p", '<p style="xxx">'),
             ("br", "<br/>"),
             ("br", "<br />"),
-            ("div", "<div\nid='whatever'>")
+            ("div", "<div\nid='whatever'>"),
         ]
 
         for node_type, buffer in cases:
-            self.assertEqual(node_type, get_node_type(buffer),
-                             f"Error for buffer='{buffer}'")
+            self.assertEqual(
+                node_type, get_node_type(buffer), f"Error for buffer='{buffer}'"
+            )
 
     def test_getting_new_node_instances(self):
         cases = [
             ("p", "<p>"),
-            ("p", "<p style=\"xxx\">"),
+            ("p", '<p style="xxx">'),
             ("br", "<br/>"),
             ("br", "<br />"),
             ("comment", "<!-- THIS IS A COMMENT -->"),
             ("comment", "<!-- THIS IS A \nMULTILINE\n COMMENT -->"),
             ("doctype", "<!doctype html>"),
             ("text", "abc"),
-            ("meta", "<meta charset=\"utf-8\" />"),
+            ("meta", '<meta charset="utf-8" />'),
         ]
 
         for node_type, buffer in cases:
             node = get_new_node(buffer)
             self.assertIsNotNone(node.type, msg=f"Error for buffer='{buffer}")
-            self.assertEqual(node_type, node.type,
-                             f"Error for buffer='{buffer}'")
+            self.assertEqual(node_type, node.type, f"Error for buffer='{buffer}'")
